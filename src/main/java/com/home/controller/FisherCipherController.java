f72b2d637541dca4d7b762481923c07122d53b18
Valentyna Svistunova <svistunova.valentyna@gmail.com>
2026-02-09 16:16:22 +0300

    Added log4j, added validation

--- src/main/java/com/home/controller/FisherCipherController.java
+++ src/main/java/com/home/controller/FisherCipherController.java
@@ -3,13 +3,18 @@ package com.home.controller;
 import com.home.dto.RequestDto;
 import com.home.model.ImageData;
 import com.home.service.FisherCipherService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/generate")
@@ -18,8 +23,8 @@ public class FisherCipherController {
     private final FisherCipherService service;
 
     @PostMapping
-    public ResponseEntity<byte[]> generate(@RequestBody RequestDto requestDto) {
-        System.out.println("requestDto = " + requestDto);
+    public ResponseEntity<byte[]> generate(@RequestBody @Valid RequestDto requestDto) {
+        log.debug("requestDto = {}", requestDto);
         ImageData imageData = service.generate(requestDto);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentDisposition(
--- src/main/java/com/home/dto/RequestDto.java
+++ src/main/java/com/home/dto/RequestDto.java
@@ -1,6 +1,8 @@
 package com.home.dto;
 
 import com.home.model.Direction;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +13,14 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RequestDto {
+
+    @NotBlank
     private String message;
     private Direction key = Direction.NONE;
+
+    @Min(10)
     private Integer lineHeight;
+
+    @Min(100)
     private Integer width;
 }
